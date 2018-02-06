fa580ed11754e70fcac8e028850cc1f84ed3dbfd
Marco Villegas Moisés <marcovillegasmoises@gmail.com>
2018-02-06 11:45:19 -0500

    WEB-549 - Crear Modulo: 'myhtmlsaved'

---
+++ cache/smarty/compile/33/7a/de/337ade031d93b3fbb4ba6700e3efc2c694df1f2a.file.warning_module.tpl.php
@@ -0,0 +1,30 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2018-02-06 10:52:32
+         compiled from "C:\xampp\htdocs\prestashop_pruebas\admin455dhvxiw\themes\default\template\controllers\modules\warning_module.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:255795a79cf405e1223-55626414%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '337ade031d93b3fbb4ba6700e3efc2c694df1f2a' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\prestashop_pruebas\\admin455dhvxiw\\themes\\default\\template\\controllers\\modules\\warning_module.tpl',
+      1 => 1504540662,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '255795a79cf405e1223-55626414',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'module_link' => 0,
+    'text' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_5a79cf40615d43_50149483',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5a79cf40615d43_50149483')) {function content_5a79cf40615d43_50149483($_smarty_tpl) {?>
+<a href="<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['module_link']->value, ENT_QUOTES, 'UTF-8', true);?>
+"><?php echo $_smarty_tpl->tpl_vars['text']->value;?>
+</a><?php }} ?>
