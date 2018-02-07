fc0f4564bf03630ef804b992fe244378276149d7
Marco Villegas Moisés <marcovillegasmoises@gmail.com>
2018-02-07 09:47:28 -0500

    Cambios

---
+++ cache/smarty/cache/productlist_colors/5/1/1/e9/5b/ea/e95bead40c61ea0f13c560e404e92c3740fa9f5b.product-list-colors.tpl.php
@@ -0,0 +1,36 @@
+<?php /*%%SmartyHeaderCode:193915a7b077b2d8f33-28350642%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e95bead40c61ea0f13c560e404e92c3740fa9f5b' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\prestashop_pruebas\\themes\\leo_exist\\product-list-colors.tpl',
+      1 => 1517862363,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '193915a7b077b2d8f33-28350642',
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_5a7b077b77aad7_01804001',
+  'has_nocache_code' => false,
+  'cache_lifetime' => 31536000,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5a7b077b77aad7_01804001')) {function content_5a7b077b77aad7_01804001($_smarty_tpl) {?>
+<ul class="color_to_pick_list clearfix">
+					<li>
+			<a href="http://localhost/prestashop_pruebas/vestidos-verano/5-vestido-verano-estampado.html#/size-s/color-negro"  class="color_pick" style="background:#434A54;">
+							</a>
+		</li>
+					<li>
+			<a href="http://localhost/prestashop_pruebas/vestidos-verano/5-vestido-verano-estampado.html#/size-s/color-naranja"  class="color_pick" style="background:#F39C11;">
+							</a>
+		</li>
+					<li>
+			<a href="http://localhost/prestashop_pruebas/vestidos-verano/5-vestido-verano-estampado.html#/size-s/color-azul"  class="color_pick" style="background:#5D9CEC;">
+							</a>
+		</li>
+					<li>
+			<a href="http://localhost/prestashop_pruebas/vestidos-verano/5-vestido-verano-estampado.html#/size-s/color-amarillo"  class="color_pick" style="background:#F1C40F;">
+							</a>
+		</li>
+	</ul><?php }} ?>
---
+++ cache/smarty/cache/productlist_colors/7/1/1/e9/5b/ea/e95bead40c61ea0f13c560e404e92c3740fa9f5b.product-list-colors.tpl.php
@@ -0,0 +1,37 @@
+<?php /*%%SmartyHeaderCode:193915a7b077b2d8f33-28350642%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e95bead40c61ea0f13c560e404e92c3740fa9f5b' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\prestashop_pruebas\\themes\\leo_exist\\product-list-colors.tpl',
+      1 => 1517862363,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '193915a7b077b2d8f33-28350642',
+  'variables' => 
+  array (
+    'colors_list' => 0,
+    'col_img_dir' => 0,
+    'color' => 0,
+    'link' => 0,
+    'img_color_exists' => 0,
+    'img_col_dir' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_5a7b077b84aa27_62702513',
+  'cache_lifetime' => 31536000,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5a7b077b84aa27_62702513')) {function content_5a7b077b84aa27_62702513($_smarty_tpl) {?>
+<ul class="color_to_pick_list clearfix">
+					<li>
+			<a href="http://localhost/prestashop_pruebas/vestidos-verano/7-vestido-estampado-gasa.html#/size-s/color-verde"  class="color_pick" style="background:#A0D468;">
+							</a>
+		</li>
+					<li>
+			<a href="http://localhost/prestashop_pruebas/vestidos-verano/7-vestido-estampado-gasa.html#/size-s/color-amarillo"  class="color_pick" style="background:#F1C40F;">
+							</a>
+		</li>
+	</ul><?php }} ?>
